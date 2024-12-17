d78aee3fdc57edec40857e5a0e995f2b4b5d19c4
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2024-12-17 14:03:46 +0530

    viteup

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api':{
-        target: 'http://localhost:3000',
+        target: 'https://job-bakend.onrender.com',
         changeOrigin: true,
       },
     },
